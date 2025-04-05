3b8e31d010b004080501cdd39d4e7a191ed7fcdf
Matheus <matheus.brgsz@gmail.com>
2025-04-04 22:58:35 -0300

    Refactor booking logic to use user-provided date and streamline slot selection process

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-from pprint import pprint
+
 
 load_dotenv()
 
@@ -11,30 +11,30 @@ headers = {
 }
 
 USERNAME = os.environ.get("USER_NAME")
-endpoint = f"https://api.cal.com//v2/slots"
-query_string = f"?eventTypeSlug=reuniao&username={USERNAME}&start=2025-04-05&end=2025-04-07&timeZone=America/Sao_Paulo"
 
+name = input("Digite seu nome: ")
+email = input("Digite seu email: ")
+date = input("Digite a data em que deseja agendar (YYYY-MM-DD): ")
+
+endpoint = "https://api.cal.com//v2/slots"
+query_string = f"?eventTypeSlug=reuniao&username={USERNAME}&start={date}&end={date}&timeZone=America/Sao_Paulo"
 url = endpoint + query_string
 
 response = requests.get(url, headers=headers)
-dates = response.json()["data"]
+dates = response.json()["data"][date]
 
 menu = {}
 
 print("Horarios disponiveis: ")
-print()
-
-for day in dates:
-
-    for index, slot in enumerate(dates[day]):
-        menu[index] = slot['start']
-        print(f"({index}) = {menu[index]}")
 
 print()
+for index, slot in enumerate(dates):
+    menu[index] = slot['start']
+    print(f"({index}) = {menu[index]}")
+
+print()
+
 selected = int(input("Escolha um horario a partir do indice: "))
-name = input("Digite seu nome: ")
-email = input("Digite seu email: ")
-
 
 url = "https://api.cal.com/v2/bookings"
 
@@ -42,7 +42,6 @@ headers = {
     "Authorization": os.environ.get("API_KEY"),
     "cal-api-version": "2024-08-13",
     "Content-Type" : "application/json"
-
 }
 
 payload = {
@@ -56,3 +55,4 @@ payload = {
 }   
 
 response = requests.post(url, json=payload, headers=headers)
+print(response.status_code)
